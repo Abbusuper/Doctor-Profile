45ec2690797264f49a9cc3eb5e5b027213cdb2a3
Abbusuper <79321049+Abbusuper@users.noreply.github.com>
2023-12-10 20:41:26 +0000

    commited

--- doctor-profile/src/background.jsx
+++ doctor-profile/src/background.jsx
@@ -4,9 +4,8 @@ import "./App.css";
 const BackgroundImg = () => {
   return (
     <div className="bgImg">
-      <div className="img1"></div>
-      <div className="img2"></div>
-      <div className="img3"></div>
+      <h1 className="backgroundHead">Doctor Details</h1>
+      <span className="backgroundRoute">Home/Doctor Details</span> 
     </div>
   );
 };
